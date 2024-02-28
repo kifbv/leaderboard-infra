008619213f0107832fd83a1c6146718eb34574f9
Franck Ratier <7999713+kifbv@users.noreply.github.com>
2024-02-28 17:17:04 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "my-table"
+  hash_key = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
